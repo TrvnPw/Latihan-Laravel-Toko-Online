41a364d25d9ad420cd5c69e0290ec41b01a4a7b1
Revan Putra Wahyu <revanputra3768@gmail.com>
2026-05-03 23:08:46 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Kategori;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::create([
+            'nama' => 'Administrator',
+            'email' => 'admin@admin.com',
+            'role' => '1',
+            'status' => 1,
+            'hp' => '0812345678901',
+            'password' => bcrypt('P@55word'),
+        ]);
+         #untuk record berikutnya silahkan, beri nilai berbeda pada nilai: nama, email, hp dengan nilai masing-masing anggota kelompok 
+        User::create([ 
+            'nama' => 'Sopian Aji', 
+            'email' => 'sopianaji@admin.com', 
+            'role' => '0', 
+            'status' => 1, 
+            'hp' => '081234567892', 
+            'password' => bcrypt('P@55word'), 
+        ]);
+        User::create([ 
+            'nama' => 'Revan', 
+            'email' => 'revan@admin.com', 
+            'role' => '0', 
+            'status' => 1, 
+            'hp' => '081234567892', 
+            'password' => bcrypt('P@55word'), 
+        ]);
+        User::create([ 
+            'nama' => 'Zackana', 
+            'email' => 'zackana@admin.com', 
+            'role' => '0', 
+            'status' => 0, 
+            'hp' => '081234567892', 
+            'password' => bcrypt('P@55word'), 
+        ]);
+        #data kategori
+        Kategori::create([
+            'nama_kategori' => 'Brownies',
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'Combro',
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'Dawet',
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'Mochi',
+        ]);
+        Kategori::create([
+            'nama_kategori' => 'Wingko',
+        ]);
+    }
+}
